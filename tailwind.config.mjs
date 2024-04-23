518d3da5090d51fd00a8bdbd06c45d09046e2c8a
Ronald Hernandez <ronaldsneyder07@gmail.com>
2024-04-23 14:12:43 -0500

    Initial commit files

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily:{
+			sans: ['Gotham SSm A, sans-serif']
+		},
+		extend: {},
+		fontSize:{
+			'4xl':'2.5rem',
+		},
+		minWidth:{
+			'50':'12.5rem'
+		}
+
+	},
+	plugins: [],
+}
